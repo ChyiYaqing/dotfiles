73db18528f9b97548631acef7f0277d49572f3b6
ChyiYaqing <chyiyaqing@gmail.com>
2025-02-24 11:56:28 +0800

    add tmux nvim vim config

---
+++ nvim/lua/plugins/misc.lua
@@ -0,0 +1,31 @@
+-- Standalone plugins with less tha 10 lines of config go here
+
+return {
+    -- Hints key binds
+    {
+      "folke/which-key.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      },
+      keys = {
+        {
+          "<leader>?",
+          function()
+            require("which-key").show({ global = false })
+          end,
+          desc = "Buffer Local Keymaps (which-key)",
+        },
+      },
+    },
+    -- Autoclose parentheses, brackets, quotes, etc.
+    {
+      'windwp/nvim-autopairs',
+      event = "InsertEnter",
+      config = true
+      -- use opts = {} for passing setup options
+      -- this is equivalent to setup({}) function
+    }
+}
